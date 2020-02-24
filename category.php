c9b46a32a1de01ce006708aeda5e9405ab9d673b
Stephen <ridingjr@gmail.com>
2020-02-23 19:33:47 -0800

    directories and sub-directories will now appear on the catalog page to make navigation more seamless.

--- category.php
+++ category.php
@@ -4,16 +4,39 @@
         <?php
         $current_url = home_url(add_query_arg(array(), $wp->request));
         $part1Cut = strpos($current_url, get_bloginfo('name'));
-        $part1 = substr($current_url, $part1Cut, strlen($current_url));
-        while ($part1 != false) {
-            echo $part1;
+        $part1 = substr($current_url, $part1Cut, strlen($current_url)); ?>
+        <a href="<?php echo get_home_url() ?>"><?php echo substr($part1, 0, strpos($part1, "/")); ?></a>
+        <?php $part1 = substr($part1, strpos($part1, "/") + 1, strlen($part1));
+        while ($part1Cut != false) {
             $part1Cut = strpos($part1, "/");
+            $partToEcho = substr($part1, 0, $part1Cut);
+            if ($partToEcho != "category" and $partToEcho != "author") {
+                $cat_id = get_cat_ID($partToEcho);
+                $cat_link = get_category_link($cat_id); ?>
+                <a href="<?php echo $cat_link ?>"><?php echo $partToEcho ?></a>
+        <?php }
             $part1 = substr($part1, $part1Cut + 1, strlen($part1));
-        }
+        } ?>
+        <a href="<?php echo get_category_link(get_cat_ID(single_cat_title('', false))); ?>"><?php single_cat_title(); ?></a>
+        <?php $parent_term_id = get_cat_ID(single_cat_title('', false)); // term id of parent term (edited missing semi colon)
+
+        $taxonomies = array(
+            'category',
+        );
+
+        $args = array(
+            'parent'         => $parent_term_id,
+            // 'child_of'      => $parent_term_id, 
+        );
+
+        $terms = get_terms($taxonomies, $args);
+        foreach ($terms as $term) { ?>
+            <a href="<?php echo get_category_link($term->term_id); ?>"><?php echo $term->name ?></a>
+        <?php }
         ?>
         <div class="category-main-category-container">
-            <h2 class="category-main-category-title"><?php echo single_cat_title() ?></h2>
-            <div class="category-main-category-description"><?php echo category_description() ?></div>
+            <h2 class="category-main-category-title"><?php single_cat_title(); ?></h2>
+            <div class="category-main-category-description"><?php echo category_description(); ?></div>
         </div>
         <?php
         while (have_posts()) {
